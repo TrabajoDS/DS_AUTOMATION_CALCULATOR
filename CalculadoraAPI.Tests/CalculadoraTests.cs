92f60ffc0253df2c116b8c052803e5ca05e9035f
Juan Antonio Rodríguez Martínez <jantoniorodma@correo.ugr.es>
2025-12-30 12:19:05 +0100

    Implementación de la suma con TDD: primer test y método Sumar

---
+++ CalculadoraAPI.Tests/CalculadoraTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using CalculadoraAPI.Controllers;
+
+namespace CalculadoraAPI.Tests;
+
+public class CalculadoraTests
+{
+    [Fact]
+    public void Sumar_DosNumeros_ReturnsSumaCorrecta()
+    {       
+        var controlador = new CalculadoraController();
+        
+        var resultado = controlador.Sumar(2, 3);
+
+        Assert.Equal(5, resultado.Value);
+    }
+}
