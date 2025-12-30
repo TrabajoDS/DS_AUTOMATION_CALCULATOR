c58b65721476f1e23a6e55582ed50f21f634b802
Juan Antonio Rodríguez Martínez <jantoniorodma@correo.ugr.es>
2025-12-30 12:57:28 +0100

    Implementación de la multiplicación con TDD: test de la multiplicación de dos números negativos

--- CalculadoraAPI.Tests/CalculadoraTests.cs
+++ CalculadoraAPI.Tests/CalculadoraTests.cs
@@ -125,8 +125,18 @@ public class CalculadoraTests
         var controlador = new CalculadoraController();
 
         var resultado = controlador.Multiplicar(7, 0);
-        
+
         Assert.Equal(0, resultado.Value);
+    }
+
+    [Fact]
+    public void Multiplicar_NumerosNegativos_ReturnsPositivo()
+    {
+        var controlador = new CalculadoraController();
+
+        var resultado = controlador.Multiplicar(-3, -4);
+        
+        Assert.Equal(12, resultado.Value);
     }
 
 }
